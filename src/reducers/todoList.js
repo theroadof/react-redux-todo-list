bf3c5edf1f759532caa938a78458fc34faf3f473
Cendeal <5205093You>
2020-08-04 16:13:19 +0800

    [react-todo-list][cendeal|teddy]feature: add todo reducer

---
+++ src/reducers/todoList.js
@@ -0,0 +1,10 @@
+
+const todoList= (state = [],action)=>{
+    switch (action.type){
+        case "ADD_TODO":
+            return [...state,action.text]
+        default:
+            return state
+    }
+}
+export default todoList
