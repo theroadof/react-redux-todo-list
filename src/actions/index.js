f01e4715682617698e18bc6d1e6f425fdf7c65ca
Cendeal <5205093You>
2020-08-04 18:23:46 +0800

    [react-todo-list][cendeal|teddy]feature: add show done todo list

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,11 @@
-import {ADD_TODO, DELETE_TODO, MARK_CANCEL, MARK_DONE} from "../constants/actionTypes";
+import {
+    ADD_TODO,
+    DELETE_TODO,
+    MARK_CANCEL,
+    MARK_DONE,
+    SHOW_ALL_TODO_LIST,
+    SHOW_DONE_TODO_LIST
+} from "../constants/actionTypes";
 
 export const addTodo = (text) => (
     {
@@ -19,4 +26,8 @@ export const markDone = (id) => ({
 export const markCancel = (id) => ({
     type: MARK_CANCEL,
     id
+})
+
+export const showDone = () => ({
+    type: SHOW_DONE_TODO_LIST
 })
--- src/reducers/todoList.js
+++ src/reducers/todoList.js
@@ -1,24 +1,26 @@
 import todoStatus from "../constants/todoStatus";
-import {ADD_TODO, DELETE_TODO, MARK_CANCEL, MARK_DONE} from "../constants/actionTypes";
+import {ADD_TODO, DELETE_TODO, MARK_CANCEL, MARK_DONE, SHOW_DONE_TODO_LIST} from "../constants/actionTypes";
 /*
 {
     text:"example todo",
     status:todoStatus.DOING
 }
  */
-const todoList= (state = [],action)=>{
-    switch (action.type){
+const todoList = (state = [], action) => {
+    switch (action.type) {
         case MARK_DONE:
-             state[action.id].status = todoStatus.DONE
+            state[action.id].status = todoStatus.DONE
             return [...state]
         case MARK_CANCEL:
             state[action.id].status = todoStatus.DOING
             return [...state]
         case ADD_TODO:
-            return [...state,{text:action.text,status: todoStatus.DOING}]
+            return [...state, {text: action.text, status: todoStatus.DOING}]
         case DELETE_TODO:
-            state.splice(action.id,1)
+            state.splice(action.id, 1)
             return [...state]
+        case SHOW_DONE_TODO_LIST:
+            return [state.filter(todo => todo.status === todoStatus.DONE)]
         default:
             return state
     }
