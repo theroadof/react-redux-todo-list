7dd8240f7aa15c7ae18c8106dc1e8e731cf350d1
theroadof <1294911641@qq.com>
2020-08-04 21:43:42 +0800

    [Show_Done_List][Teddy]feature:implement go to done page by using router

--- src/App.js
+++ src/App.js
@@ -3,17 +3,30 @@ import './App.css';
 import TodoFormContainer from "./containers/TodoFormContainer";
 import TodoListContainer from "./containers/TodoListContainer";
 import DoneListContainer from "./containers/DoneListContainer";
+import {HashRouter, Link, Route} from "react-router-dom";
 
 function App() {
     return (
-            <div className="App">
-                <header className="App-header">
-                    <h2>Todo List</h2>
-                    <TodoListContainer/>
-                    <TodoFormContainer/>
-                    <DoneListContainer/>
-                </header>
-            </div>
+        <React.Fragment>
+            <header className="App-header">
+                <p>Todo List</p>
+                <HashRouter>
+                    <ul>
+                        <li className="App-link"><Link to={"/done-list"}>go to done page</Link></li>
+                        <li className="App-link"><Link to={"/"}>show all list</Link></li>
+                    </ul>
+                    <Route path={"/done-list"} component={DoneListContainer}/>
+                    <Route exact path={"/"} render={()=>{
+                        return (
+                            <div>
+                                <TodoListContainer/>
+                                <TodoFormContainer/>
+                            </div>
+                        )
+                    }}/>
+                </HashRouter>
+            </header>
+        </React.Fragment>
     );
 }
 
--- src/components/DoneList/index.jsx
+++ src/components/DoneList/index.jsx
@@ -5,7 +5,6 @@ class DoneList extends React.Component {
     render() {
         return (
             <div className={"App"}>
-                <header className={"App-header"}>Todo List</header>
                 {this.props.doneList.map((todo, index) =>
                     <TodoContainer key={index} id={index} status={todo.status} text={todo.text}/>)}
             </div>
