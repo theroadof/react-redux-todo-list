f20827de04ed4165a0a31ee3aa93e9721c71e18e
Cendeal <5205093You>
2020-08-04 16:19:57 +0800

    [react-todo-list][cendeal|teddy]feature: add TodoListContainer

---
+++ src/containers/TodoListContainer.jsx
@@ -0,0 +1,13 @@
+import TodoList from "../components/TodoList";
+
+const {connect} = require("react-redux");
+
+
+const mapStateToProps=(state) =>{
+    return {
+        todoList: state.todoList
+    }
+}
+
+const TodoListContainer = connect(mapStateToProps)(TodoList)
+export default TodoListContainer
