8116e062100f5afc4d4c52a9b0a08ea69e99d5fd
Cendeal <5205093You>
2020-08-04 16:04:41 +0800

    [react-todo-list][cendeal|teddy]feature: add TodoList component

---
+++ src/components/TodoList/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from 'prop-tpyes'
+import Todo from "../Todo";
+
+class TodoList extends React.Component {
+    render() {
+        return (<div className={"todo-list"}>
+            {this.props.todoList.map(
+                (todo, index) => <Todo key={index} text={todo}/>
+            )}
+        </div>)
+    }
+}
+
+TodoList.propTypes = {
+    todoList: PropTypes.array.isRequired
+}
+export default TodoList;
