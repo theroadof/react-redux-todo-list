b77c764e5a2e510d0f92811732c27db9efd62863
theroadof <1294911641@qq.com>
2020-08-05 15:19:32 +0800

    [axios][Teddy]refactor:add CloseOutlined icon to todo

--- src/App.js
+++ src/App.js
@@ -5,36 +5,44 @@ import TodoListContainer from "./containers/TodoListContainer";
 import DoneListContainer from "./containers/DoneListContainer";
 import {HashRouter, Link, Route} from "react-router-dom";
 import AllTodosContainer from "./containers/AllTodosContainer";
-import {Menu, PageHeader} from "antd";
+import {Menu, PageHeader, Layout} from "antd";
 import 'antd/dist/antd.css';
+
+const {Header, Footer, Sider, Content} = Layout;
 
 function App() {
     return (
         <React.Fragment>
             <HashRouter>
-                <PageHeader title={"Todo List"}>
+                <Layout>
+                    <PageHeader title={"Todo List"}>Welcome!</PageHeader>
                     <ul>
                         <li className="App-link"><Link to={"/done-list"}>go to done page</Link></li>
                         <li className="App-link"><Link to={"/"}>go to list page</Link></li>
                         <li><Link to={"/all-list"}>get all list</Link></li>
                     </ul>
-                </PageHeader>
-                <Route path={"/done-list"} component={DoneListContainer}/>
-                <Route exact path={"/"} render={() => {
-                    return (
-                        <div>
-                            <TodoListContainer/>
-                            <TodoFormContainer/>
-                        </div>
-                    )
-                }}/>
-                <Route path={"/all-list"} render={() => {
-                    return (
-                        <div>
-                            <AllTodosContainer/>
-                        </div>
-                    )
-                }}/>
+
+                    <Footer>
+                        <Route path={"/done-list"} component={DoneListContainer}/>
+                        <Route exact path={"/"} render={() => {
+                            return (
+                                <div>
+                                    <TodoListContainer/>
+                                    <TodoFormContainer/>
+                                </div>
+                            )
+                        }}/>
+                        <Route path={"/all-list"} render={() => {
+                            return (
+                                <div>
+                                    <AllTodosContainer/>
+                                </div>
+                            )
+                        }}/>
+                    </Footer>
+
+
+                </Layout>
             </HashRouter>
         </React.Fragment>
     );
